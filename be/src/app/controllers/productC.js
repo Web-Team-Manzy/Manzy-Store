a6e83149fcf3a5c922d76f3afca2bc1585de280a
22120216 <22120216@student.hcmus.edu.vn>
2024-12-16 21:23:41 +0700

    create Product

---
+++ be/src/app/controllers/productC.js
@@ -0,0 +1,93 @@
+const productM = require('../models/productM'); 
+const cloudinary = require('../../config/cloud/clConfig');
+
+class productC {
+
+    async addProduct(req, res) {
+        try {
+            
+            const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
+
+            const image1= req.files.image1 && req.files.image1[0];
+            const image2= req.files.image2 && req.files.image2[0];
+            const image3= req.files.image3 && req.files.image3[0];
+            const image4= req.files.image4 && req.files.image4[0];
+
+            const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+
+            let imagesUrl = await Promise.all(
+                images.map(async (pathitem) => {
+                    let result = await cloudinary.uploader.upload(pathitem.path, {resource_type: "image"});
+                    return result.secure_url;
+                })
+            );
+
+            const productData = {
+                name,
+                description,
+                price: Number(price),
+                category,
+                subCategory,
+                sizes: JSON.parse(sizes),
+                bestseller: bestseller === 'true'? true : false,
+                images: imagesUrl
+            };
+
+            const product = new productM(productData);
+            await product.save();
+
+            res.json({success: true, message: "Add product successfully"});
+
+        } catch (error) {
+            console.log(error);
+            res.json({success: false, message: error.message});
+        }
+
+    }
+
+    async listProduct(req, res) {
+        try {
+            const products = await productM.find({});
+            res.json({success: true, products});
+        } catch (error) {
+            console.log(error);
+            res.json({success: false, message: error.message});
+        }
+    }
+
+    async detailProduct(req, res) {
+        try {
+            
+            const {productId} = req.body;
+            const product = await productM.findById(productId);
+            res.json({success: true, product});
+
+        } catch (error) {
+            console.log(error);
+            res.json({success: false, message: error.message});
+        }
+    }
+
+    async updateProduct(req, res) {
+
+    }
+
+    async deleteProduct(req, res) {
+        try {
+            
+            await productM.findByIdAndDelete(req.query.id);
+            res.json({success: true, message: "Delete product successfully"});
+
+        } catch (error) {
+            console.log(error);
+            res.json({success: false, message: error.message});
+        }
+    }
+    
+}
+
+module.exports = new productC();
+
+
+
+
--- be/src/config/db/index.js
+++ be/src/config/db/index.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 async function connect() {
     try {
         await mongoose.connect(
-            "mongodb+srv://hoanglenam0905:22120217@cluster0.5queg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb+srv://hoanglenam0905:22120217@cluster0.5queg.mongodb.net/ManzyStoreDB?retryWrites=true&w=majority&appName=Cluster0"
         );
         console.log("Connect successfully!!!");
     } catch (error) {
+        console.log(error);
         console.log("Connect failure!!!");
     }
 }
