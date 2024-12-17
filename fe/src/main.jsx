c957da81a4b69cc79fc24a1f1e901b5c02c0d75c
22120217 <hoanglenam0905@gmail.com>
2024-12-18 02:08:05 +0700

    [+] setup redux for frontend

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,13 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
-  </BrowserRouter>,
-)
+import App from "./App.jsx";
+import ShopContextProvider from "./context/ShopContext.jsx";
+import store from "./redux/store.js";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <ShopContextProvider>
+                <App />
+            </ShopContextProvider>
+        </BrowserRouter>
+    </Provider>
+);
