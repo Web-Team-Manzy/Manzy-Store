72f86350c5c57a2b25d4e15185941c64082df9d8
21120515 <147778709+TranPhuocNhan@users.noreply.github.com>
2024-12-22 04:33:12 +0700

    feat: adding add page, list page, create login, navbar, sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Order";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import Login from "./components/Login";
+import "react-toastify/dist/ReactToastify.css";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "$";
 
 const App = () => {
-  return <div></div>;
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add setToken={setToken} />} />
+                <Route path="/list" element={<List setToken={setToken} />} />
+                <Route
+                  path="/orders"
+                  element={<Orders setToken={setToken} />}
+                />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
