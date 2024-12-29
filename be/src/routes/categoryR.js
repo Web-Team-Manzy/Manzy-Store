227ea9306bc7af37d7a30d4ba6d8ade095f263f8
22120217 <hoanglenam0905@gmail.com>
2024-12-29 23:48:00 +0700

    add middleware

--- be/src/routes/categoryR.js
+++ be/src/routes/categoryR.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const categoryC = require("../app/controllers/categoryC");
 
-router.get("/list", categoryC.listCategory);
-router.post("/add", categoryC.addCategory);
-router.put("/update", categoryC.updateCategory);
+const auth = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
 
-module.exports = router;    
+router.get("/list", categoryC.listCategory);
+router.post("/add", auth, isAdmin, categoryC.addCategory);
+router.put("/update", auth, isAdmin, categoryC.updateCategory);
+
+module.exports = router;
--- be/src/routes/productR.js
+++ be/src/routes/productR.js
@@ -3,23 +3,28 @@ const router = express.Router();
 const productC = require("../app/controllers/productC");
 const upload = require("../middleware/multer");
 
+const auth = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
+
 router.get("/list", productC.listProduct);
 
 router.post(
-  "/add",
-  upload.fields([
-    { name: "image1", maxCount: 1 },
-    { name: "image2", maxCount: 1 },
-    { name: "image3", maxCount: 1 },
-    { name: "image4", maxCount: 1 },
-  ]),
-  productC.addProduct
+    "/add",
+    auth,
+    isAdmin,
+    upload.fields([
+        { name: "image1", maxCount: 1 },
+        { name: "image2", maxCount: 1 },
+        { name: "image3", maxCount: 1 },
+        { name: "image4", maxCount: 1 },
+    ]),
+    productC.addProduct
 );
 
 router.get("/detail/:productId", productC.detailProduct);
 
-router.put("/update", productC.updateProduct);
+router.put("/update", auth, isAdmin, productC.updateProduct);
 
-router.delete("/delete", productC.deleteProduct);
+router.delete("/delete", auth, isAdmin, productC.deleteProduct);
 
 module.exports = router;
