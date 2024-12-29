ebfb53e626e30b9b7590dd6e992da150ab3ac260
22120217 <hoanglenam0905@gmail.com>
2024-12-30 01:15:32 +0700

    add createAccount in controller

---
+++ payment_system/src/app/controllers/payment.controller.js
@@ -0,0 +1,37 @@
+const Account = require("../models/account.model");
+
+class PaymentController {
+    async createAccount(req, res, next) {
+        try {
+            const { userId } = req.body;
+
+            const existedAccount = await Account.findOne({ userId });
+
+            if (existedAccount) {
+                return res.status(400).json({
+                    EC: 1,
+                    EM: "Account existed",
+                });
+            }
+
+            const newAccount = new Account({
+                userId,
+            });
+
+            await newAccount.save();
+
+            return res.status(200).json({
+                EC: 0,
+                EM: "Create account successfully",
+            });
+        } catch (error) {
+            console.log(">>> PaymentController.createAccount", error);
+            return res.status(500).json({
+                EC: 1,
+                EM: "Internal Server Error",
+            });
+        }
+    }
+}
+
+module.exports = new PaymentController();
