97fa4dd28a8d9822d2cd2137ac1000294249fb49
22120216 <22120216@student.hcmus.edu.vn>
2024-12-30 10:19:47 +0700

    update filter bestseller, Latest product list

--- be/src/app/controllers/cartC.js
+++ be/src/app/controllers/cartC.js
@@ -1,22 +1,31 @@
-const { source } = require('../../config/cloud/clConfig');
 const userM = require('../models/userM'); 
+const productM = require('../models/productM');
 
 class cartC {
+
     async getUserCart(req, res) {
         try {
-            
-            const {userId} = req.body;
-
+            const { userId } = req.body;
+            console.log(userId);
+    
             const userData = await userM.findById(userId);
             let cartData = await userData.cartData;
-
-            res.json({success: true, cartData});
-
+    
+            const detailedCartData = await Promise.all(Object.keys(cartData).map(async (itemId) => {
+                const product = await productM.findById(itemId);
+                const sizes = cartData[itemId];
+                return {
+                    product,
+                    sizes
+                };
+            }));
+    
+            res.json({ success: true, cartData: detailedCartData });
+    
         } catch (error) {
             console.log(error);
-            res.json({success: false, message: error.message});    
+            res.json({ success: false, message: error.message });
         }
-
     }
 
     async addToCart(req, res) {
