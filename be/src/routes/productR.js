a1c76794d5378ba575029ad4e4614fc74c5518b1
22120216 <22120216@student.hcmus.edu.vn>
2025-01-03 21:50:04 +0700

    fix product, related

--- be/src/routes/productR.js
+++ be/src/routes/productR.js
@@ -2,24 +2,23 @@ const express = require("express");
 const router = express.Router();
 const productC = require("../app/controllers/productC");
 const upload = require("../middleware/multer");
-
 const auth = require("../middleware/auth");
-const isAdmin = require("../middleware/isAdmin");
+const admin = require("../middleware/isAdmin");
 
 router.get("/list", productC.listProduct);
 
 router.post(
-    "/add",
-    auth,
-    isAdmin,
-    upload.fields([
-        { name: "image1", maxCount: 1 },
-        { name: "image2", maxCount: 1 },
-        { name: "image3", maxCount: 1 },
-        { name: "image4", maxCount: 1 },
-    ]),
-    productC.addProduct
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),  auth, admin,
+  productC.addProduct
 );
+
+router.get("/Related/:productId", productC.getRelatedProducts);
 
 router.get("listBestSeller", productC.listBestSeller);
 
@@ -27,8 +26,8 @@ router.get("/listNewProduct", productC.listNewProduct);
 
 router.get("/detail/:productId", productC.detailProduct);
 
-router.put("/update", auth, isAdmin, productC.updateProduct);
+router.put("/update", auth, admin, productC.updateProduct);
 
-router.delete("/delete", auth, isAdmin, productC.deleteProduct);
+router.delete("/delete", auth, admin, productC.deleteProduct);
 
 module.exports = router;
